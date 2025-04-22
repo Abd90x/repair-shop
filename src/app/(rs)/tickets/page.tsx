63806cfa85284a2c1f2ef4e2eec990554bbbf758
Abdellatif Alrefahi <abd90v@gmail.com>
2025-04-22 17:28:22 +0300

    initial pages create

---
+++ src/app/(rs)/tickets/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Tickets",
+};
+
+const Tickets = () => {
+  return <h2>Tickets Page</h2>;
+};
+
+export default Tickets;
