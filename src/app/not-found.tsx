ee08e79d4184425ebba37ee99da17128b3a8f706
Abdellatif Alrefahi <abdellatif.alrefahi@gmail.com>
2025-05-04 23:25:40 +0300

    style(not-found): improve UI of not found page with centered layout and styling
    Enhance the not-found page by centering the content, adding a ship icon, and using a styled button for the home link. This improves the visual appeal and user experience.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,16 @@
+import { Button } from "@/components/ui/button";
+import { Ship } from "lucide-react";
 import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
+    <div className="w-full h-dvh flex flex-col items-center justify-center gap-4">
+      <Ship className="size-32" />
+      <h2 className="text-5xl font-bold">Page Not Found</h2>
       <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+      <Link href="/tickets">
+        <Button>Go Home</Button>
+      </Link>
     </div>
   );
 }
