ce5ce840de3aa8b328d6796bb968851a0b1b76f8
Abdellatif Alrefahi <abdellatif.alrefahi@gmail.com>
2025-05-04 20:18:36 +0300

    feat(customers): add customer table with react-table and improve search results display
    Introduce a new customer table component using @tanstack/react-table to display search results in a structured format. Enhance the search results page by add responsive table layout. Additionally, optimize the search query by concatenating first and last names for better matching.

--- src/app/(rs)/customers/page.tsx
+++ src/app/(rs)/customers/page.tsx
@@ -1,6 +1,9 @@
 import { Metadata } from "next";
 import CustomerSearch from "./CustomerSearch";
 import { getCustomerSearchResults } from "@/lib/queries/getCustomerSearchResults";
+import * as Sentry from "@sentry/nextjs";
+import CustomerTable from "./CustomerTable";
+
 export const metadata: Metadata = {
   title: "Customers",
 };
@@ -14,14 +17,23 @@ const Customers = async ({
 
   if (!searchText) return <CustomerSearch />;
 
+  const span = Sentry.startInactiveSpan({
+    name: "getCustomerSearchResults-1",
+  });
   const results = await getCustomerSearchResults(searchText);
 
+  span.end();
+
   return (
-    <>
+    <div className="flex flex-col gap-5">
       <CustomerSearch />
-      <h2 className="text-2xl mb-2">Search Results</h2>
-      {JSON.stringify(results)}
-    </>
+      <h2 className="text-2xl">Search Results</h2>
+      {results.length ? (
+        <CustomerTable data={results} />
+      ) : (
+        <p>No results found</p>
+      )}
+    </div>
   );
 };
 
